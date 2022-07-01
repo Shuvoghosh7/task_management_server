6b1138dedd4c16f4353e7fa0d2fac8e4fe3486d9
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-07-01 07:46:54 +0600

    create update api for update task

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json()) //use to get data req.body
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2ptrm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
@@ -23,13 +23,39 @@ async function run() {
             const task = await cursor.toArray();
             res.send(task);
         })
-        
+        //get single task
+        app.get('/get-task/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const parts = await taskCollection.findOne(query);
+            res.send(parts)
+          });
         //add task
         app.post('/add-task', async (req, res) => {
             const taskInfo = req.body
             const result = await taskCollection.insertOne(taskInfo)
             res.send(result)
         });
+
+        //update task
+        app.put("/update-task/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const data = req.body;
+            console.log(data)
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+              $set: {
+                task:data.task
+              },
+            };
+            const result = await taskCollection.updateOne(filter,updateDoc,options);
+            res.send(result);
+          });
+
+          
+       
     }
     finally {
 
